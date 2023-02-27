ed3db11d753eccf901fcf2d25da5dc198e3ee81f
Mitesh <mits50@gmail.com>
2023-02-26 22:16:15 -0500

    added tailwind, bootstrap added model, style form,

--- src/App.js
+++ src/App.js
@@ -1,5 +1,55 @@
+import { useState } from "react";
+import Employee from "./components/Employee";
+import "./Index.css";
+import { v4 as uuidv4 } from "uuid";
+
 function App() {
-  return <h1>Full stack</h1>;
+  const [employees, setEmployees] = useState([
+    {
+      name: "Mak",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+    },
+    {
+      name: "Mak",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+    },
+    {
+      name: "Mak",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+    },
+    {
+      name: "Mak",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+    },
+    {
+      name: "Mak",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+    },
+    {
+      name: "Mak",
+      role: "developer",
+      img: "https://images.pexels.com/photos/2381069/pexels-photo-2381069.jpeg",
+    },
+  ]);
+  return (
+    <div className="flex flex-wrap justify-center">
+      {employees.map((employee) => {
+        return (
+          <Employee
+            key={uuidv4()}
+            name={employee.name}
+            role={employee.role}
+            img={employee.img}
+          />
+        );
+      })}
+    </div>
+  );
 }
 
 export default App;
